1e9938440496ce575280bf47bd9743abb9d93bd2
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2022-03-21 21:40:03 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (94.03%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    bool isHappy(int n) {
+        
+        int sum=0;
+        while(n/10 >0 ){
+          sum=0;
+          while(n != 0){
+            sum+=(n%10)*(n%10);
+            n/=10;
+          }
+          n=sum;
+        }
+         if(n==1 || n==7)
+           return true;
+         else 
+           return false;
+    }
+};
