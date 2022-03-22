3366b8aecdc40b5af44d88d8088e0b131a72e693
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2022-03-22 19:02:10 +0530

    Time: 118 ms (29.79%), Space: 43.5 MB (80.20%) - LeetHub

--- 258-add-digits/258-add-digits.js
+++ 258-add-digits/258-add-digits.js
@@ -10,3 +10,17 @@ var addDigits = function(num) {
     else
        return num%9;
 };
+
+/*The logic behind this approach , I think is related to the divisibility test of 9.
+If you don't know then , "To check if a number is divisible by 9, add the digits of the number and check if the sum is divisible by 9 or not. If yes , is the case , then the number is divisible by 9 , otherwise it's not ."
+Now if you notice if a number is divisible by 9(and 3) , then if we follow the procedure as mention in the question (sum of digits of every previous num) , we would always find the new_num(the sum of the digits ) to be divisible by 9(and 3).
+
+{
+sum=addDigits(num);
+if num%9==0 , then sum%9==0 too.
+Now, num=sum;
+Since sum%9=0 , so ,
+sum=addDigits(sum) %9 =0
+}
+
+But , if the number is not divisible by 9 , if you continue to evaluate the sum of digits , the sum generated will be a number which when divided by 9 will generate the same remainder as it would have if we wold have divided num. So , if we see , then the remainder of num , doesn't change with sum of digits. */
