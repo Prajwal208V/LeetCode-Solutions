012dbcc3657e9254624e92977df5c63ef579f9c3
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2022-09-04 22:22:45 +0530

    Time: 283 ms (94.06%), Space: 144.9 MB (25.15%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,25 +12,16 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        //If we have no tree then min depth is o
-        if(root == nullptr) {
-            return 0;
-        }
-        //If tree has one node only then min depth is 1
-        if(root->left == nullptr and root->right == nullptr) {
-            return 1;
-        }
-        //If tree only has right subtree and no left subtree
-        if(root->left == nullptr) {
-            return 1 + minDepth(root->right);
-        }
-        //If tree only has left subtree and no right subtree
-        if(root->right == nullptr) {
-            return 1 + minDepth(root->left);
-        }
-        //If tree has both left and right subtree present
-        else {
-            return 1 + min(minDepth(root->left), minDepth(root->right));
-        }
+     if(root==nullptr)
+         return 0;
+     if(root->left==nullptr && root->right==nullptr)
+         return 1;
+     if(root->left==nullptr)
+         return 1+minDepth(root->right);
+     if(root->right==nullptr)
+         return 1+minDepth(root->left);
+   
+    else 
+         return 1+min(minDepth(root->left),minDepth(root->right));
     }
 };
