5501235e3507d1efda9d7ec7c8a506c1ccc892be
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2025-08-29 11:21:12 +0530

    [1] [Time Beats: 100.00%] [Memory Beats: 23.31%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hasmap = {}
-        
-        for ind,num in enumerate(nums):
+        seen = {}
+
+        for i,num in enumerate(nums):
             complement = target - num
-            if  complement in hasmap:
-                return [hasmap[complement], ind]
-            hasmap[num] = ind
-            
-            
-        
+            if complement in seen:
+                return [seen[complement],i]
+            seen[num] = i
